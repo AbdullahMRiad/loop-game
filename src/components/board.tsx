1d9bf44fdf52e3a4bd4469df3dfc25e510862d40
AbdullahMRiad <abdullahmriad@gmail.com>
2025-07-04 01:27:52 +0300

    Add board rendering (with sample board)

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -4,16 +4,18 @@ import Square from "./square";
 export default function Board() {
     const DEBUG_BOARD: Game.Board = [["X", "O", "X"], ["X", "O", "X"], ["X", "O", "X"]]
     return(
-        DEBUG_BOARD.map(
-            (row, rowIndex) => (
-                <div key={rowIndex}>
-                    {row.map(
-                        (cell, cellIndex) => (
-                            <Square key={cellIndex} content={cell}/>
-                        )
-                    )}
-                </div>
-            )
-        )
+        <>
+            {DEBUG_BOARD.map(
+                (row, rowIndex) => (
+                    <div key={rowIndex}>
+                        {row.map(
+                            (cell, cellIndex) => (
+                                <Square key={cellIndex} content={cell}/>
+                            )
+                        )}
+                    </div>
+                )
+            )}
+        </>
     )
 }
