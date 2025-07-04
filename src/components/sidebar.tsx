7850522834237063d3fe6cc02038814b51b5dc2e
AbdullahMRiad <abdullahmriad@gmail.com>
2025-07-04 17:18:45 +0300

    Changed sidebar appear transition

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,7 +2,7 @@ import Button from "./ui/button";
 
 export default function Sidebar({opened}:{opened:boolean}) {
     return(
-        <div className={`fixed transition-all h-full overflow-hidden duration-300 ease-in-out w-64 ${opened ? "left-0 scale-100" : "-left-full -scale-100"}`}>
+        <div className={`fixed transition-all h-full overflow-hidden duration-500 ease-bounce ${opened ? "left-0 scale-100 opacity-100" : "-left-full scale-75 opacity-0"}`}>
             <ul className="flex flex-col items-center m-4 mt-24 p-3 outline-4 rounded-xl h-6/9 overflow-y-scroll">
                 <h4 className="z-10 text-2xl font-semibold">Move list</h4>
                 {Array.from(Array(10).keys()).map(
